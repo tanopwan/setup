45e748154596b08d9e42c705b0907a9374082808
Tanopwan Wongpichitkun <tanopwan.won@ascendcorp.com>
2019-11-27 13:34:07 +0700

    quick commit

--- alias/git.sh
+++ alias/git.sh
@@ -6,4 +6,4 @@ git config --global alias.s status
 git config --global alias.p push
 git config --global alias.fuckit 'reset --hard'
 git config --global alias.pp '!git fetch && git pull'
-git config --global alias.ee '!git add -A && git commit -m \"quick commit\"" . && git push'
+git config --global alias.ee '!git add -A && git commit -m \"quick commit\"" . && git push origin'
