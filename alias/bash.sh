9a1d9258f080b16505f14b9a65f1bc552a2f27b9
Tanopwan Wongpichitkun <tanopwan.won@ascendcorp.com>
2019-12-11 16:39:38 +0700

    pull request bitbucket

--- alias/bash.sh
+++ alias/bash.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-touch .bash_profile
-echo "alias k=kubectl" >>~/.bash_profile
-echo "alias c2=connectToEC2" >>~/.bash_profile
-echo 'export GOPATH=${HOME}/go' >>~/.bash_profile
-echo 'export GOROOT="$(brew --prefix golang)/libexec"' >>~/.bash_profile
-echo 'export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"' >>~/.bash_profile
-echo 'alias ppp="find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pp \;"' >>~/.bash_profile
+touch .zprofile
+echo "alias g=git" >>~/.zprofile
+echo "alias k=kubectl" >>~/.zprofile
+echo "alias c2=connectToEC2" >>~/.zprofile
+echo 'export GOPATH=${HOME}/go' >>~/.zprofile
+echo 'export GOROOT="$(brew --prefix golang)/libexec"' >>~/.zprofile
+echo 'export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"' >>~/.zprofile
+echo 'alias ppp="find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pp \;"' >>~/.zprofile
--- bash/function.sh
+++ bash/function.sh
@@ -2,4 +2,31 @@ connectToEC2() {
     ssh -i ~/.ssh/tmn-th-nonprod-keypair.pem "centos@$1"
 }
 
+pullRequest() {
+    repo=${PWD##*/}
+    curl -X POST \
+    -u tanopwan_won:xxx \
+    -H "Content-Type: application/json" \
+    https://api.bitbucket.org/2.0/repositories/anycorp/$repo/pullrequests \
+    -d '{
+        "title": "Perseus Merge to Master",
+        "description": "Quick Pull Request",
+        "source": {
+            "branch": {
+                "name": "perseusbranch"
+            },
+            "repository": {
+                "full_name": "anycorp/$repo"
+            }
+        },
+        "destination": {
+            "branch": {
+                "name": "master"
+            }
+        },
+        "reviewers": [ { "username": "tanopwan_won" } ],
+        "close_source_branch": true
+    }'
+}
+
 connectToEC2 10.230.18.166
