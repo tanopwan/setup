156f8670a279ac04edcb833cd074ca9d238e3e47
Tanopwan Wongpichitkun <tanopwan.won@ascendcorp.com>
2019-12-12 10:36:13 +0700

    quick commit

--- alias/git.sh
+++ alias/git.sh
@@ -5,7 +5,7 @@ git config --global alias.a 'add -A'
 git config --global alias.s status
 git config --global alias.p push
 git config --global alias.fuckit 'reset --hard'
-git config --global alias.pp '!git fetch && git pull'
+git config --global alias.pp '!git fetch --prune && git pull'
 git config --global alias.eee '!git add -A && git commit -m "quick commit" . && git push'
 git config --global alias.ee '!git add -A && git commit'
 git config --global alias.m2b '!git checkout master && git fetch --prune && git pull && git branch perseusbranch && git checkout && git checkout perseusbranch'
--- bash/function.sh
+++ bash/function.sh
@@ -2,6 +2,13 @@ connectToEC2() {
     ssh -i ~/.ssh/tmn-th-nonprod-keypair.pem "centos@$1"
 }
 
+build() {
+    curl -X POST -u "tanopwan_won:dontforget" \
+    "http://jenkins-ecs.tmn-dev.com:8080/job/"$env_name"_"$service_name"_ecs-build/buildWithParameters?app_name="$service_name"&middleware_name=jdk1.8.0_45&env_name="$env_name"&latestTag=1.0.0"
+
+    curl -X POST -u "tanopwan_won:dontforget" \
+    "http://jenkins-ecs.tmn-dev.com:8080/job/alpha_acquiring_order_ecs-build/buildWithParameters?app_name=acquiring_order&middleware_name=jdk1.8.0_45&env_name=alpha&latestTag=1.0.0"
+}
 pullRequest() {
     repo=${PWD##*/}
     curl -X POST \
